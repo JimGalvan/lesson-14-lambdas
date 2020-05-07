d47e06de90d11f75a9f369c1592d7e82f1b4761f
JimGalvan <58128571+JimGalvan@users.noreply.github.com>
2020-05-06 21:22:54 -0700

    JobCandidate and Writting lambdas finished

--- src/exercises/_3_JobCandidateSelectorUsingLambdas.java
+++ src/exercises/_3_JobCandidateSelectorUsingLambdas.java
@@ -1,14 +1,25 @@
 package exercises;
 
+import java.util.Collections;
+
 public class _3_JobCandidateSelectorUsingLambdas {
 	public static void main(String[] args) {
 
-		// 1. Sort the candidates by salary requirements, low to high.
+		// 1. Sort the candidates by salary requirements, low to high
+		Collections.sort(JobCandidate.jobCandidates,
+				(JobCandidate o1, JobCandidate o2) -> o1.getSalaryRequired() - o2.getSalaryRequired());
 		
+		System.out.println("By salary: " + JobCandidate.jobCandidates+ "\n");
+
 
 		// 2. Filter out any candidates that have not been taught by me (June)
 		// Hint: see the removeIf() method in the Collections class
+
+		JobCandidate.jobCandidates
+				.removeIf((JobCandidate person) -> person.getTeacherName().matches("^(?!.*(June)).*$"));
 		
-		
+		System.out.println("June students: " + JobCandidate.jobCandidates);
+
 	}
+
 }
